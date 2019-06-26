79fa59c54c9536af1874962111d8a873da727ed7
Eric Chauvin <chauvineric4@gmail.com>
2019-06-26 09:53:03 -0600

    Create LayoutSimpleVertical.java

---
+++ LayoutSimpleVertical.java
@@ -0,0 +1,158 @@
+
+
+import java.awt.LayoutManager;
+import java.awt.Container;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.AWTError;
+import java.awt.Insets;
+import java.awt.Rectangle;
+
+
+
+public class LayoutSimpleVertical implements LayoutManager
+  {
+  private Container target;
+  public static final int FixedHeightMax = 1000000;
+
+
+
+  public LayoutSimpleVertical( Container target )
+    {
+    this.target = target;
+    }
+
+
+
+  private void checkContainer( Container target )
+    {
+    if( this.target != target )
+      {
+      throw new AWTError( "LayoutSimple checkContainer() targets don't match." );
+      }
+    }
+
+
+
+  // This is part of the LayoutManager interface.
+  public void addLayoutComponent( String name, Component comp )
+    {
+    // Not used.
+    }
+
+
+
+
+  // This is part of the LayoutManager interface.
+  public void removeLayoutComponent( Component comp )
+    {
+    // Not used.
+    }
+
+
+
+
+  // This is part of the LayoutManager interface.
+  public Dimension preferredLayoutSize( Container target )
+    {
+    // The preferred size that this returns is the size that
+    // it is now.
+
+    checkContainer( target );
+
+    Dimension targetSize = target.getSize();
+    Insets insets = target.getInsets();
+
+    targetSize.width = targetSize.width + insets.left + insets.right;
+    targetSize.height = targetSize.height + insets.top + insets.bottom;
+
+    return targetSize;
+    }
+
+
+
+
+  // This is part of the LayoutManager interface.
+  public Dimension minimumLayoutSize( Container target )
+    {
+    return preferredLayoutSize( target );
+    }
+
+
+
+
+  // This is part of the LayoutManager interface.
+  public void layoutContainer( Container target )
+    {
+    // synchronized( this )
+      // {
+
+    checkContainer( target );
+
+    Dimension targetSize = target.getSize();
+    Insets in = target.getInsets();
+    targetSize.width -= in.left + in.right;
+    targetSize.height -= in.top + in.bottom;
+
+    int fHeight = getFixedHeight( target );
+    int leftOverHeight = targetSize.height - fHeight;
+
+    int y = in.top;
+    int n = target.getComponentCount();
+    for( int i = 0; i < n; i++ )
+      {
+      Component c = target.getComponent( i );
+      // if( !c.isVisible() )
+
+      // Dimension min = c.getMinimumSize();
+      Dimension pref = c.getPreferredSize();
+      // Dimension max = c.getMaximumSize();
+
+      int x = in.left;
+      int height = pref.height;
+      if( height >= FixedHeightMax )
+        height = leftOverHeight;
+
+      int width = targetSize.width;
+      c.setBounds( x, y, width, height );
+      y += height;
+      }
+    }
+
+
+
+
+  private int getFixedHeight( Container target )
+    {
+    int howMany = 0;
+    int total = 0;
+    int n = target.getComponentCount();
+    for( int i = 0; i < n; i++ )
+      {
+      Component c = target.getComponent( i );
+      // if( !c.isVisible() )
+
+      Dimension pref = c.getPreferredSize();
+
+      int height = pref.height;
+      if( height < FixedHeightMax )
+        {
+        total += height;
+        }
+      else
+        {
+        // Then it's a stretch object.
+        howMany++;
+        if( howMany > 1 )
+          throw new AWTError( "LayoutSimpleVertical can't have more than one stretch component." );
+
+        }
+      }
+
+    return total;
+    }
+
+
+
+
+}
