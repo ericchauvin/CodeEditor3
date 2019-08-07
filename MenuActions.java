a997f7dd3556efd73178d9d7940998f83117d0e2
Eric Chauvin <chauvineric4@gmail.com>
2019-08-07 14:36:13 -0600

    Create MenuActions.java

---
+++ MenuActions.java
@@ -0,0 +1,38 @@
+// Copyright Eric Chauvin 2019.
+
+
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+
+
+
+public class MenuActions implements ActionListener
+  {
+  private MainApp mApp;
+
+
+
+  public MenuActions( MainApp useApp )
+    {
+    mApp = useApp;
+    }
+
+
+
+  public void actionPerformed( ActionEvent e )
+    {
+    String command = e.getActionCommand();
+    mApp.showStatus( "ActionEvent Command is: " + command );
+
+    if( command == "FileSaveAll" )
+      {
+      mApp.SaveAllFiles();
+      }
+
+    }
+
+
+
+  }
