64e0a8881ef2430f8bf0f7ced1edec3943a8adbc
Eric Chauvin <chauvineric4@gmail.com>
2019-08-24 15:02:30 -0600

    Update RunApp.bat

--- RunApp.bat
+++ RunApp.bat
@@ -1,15 +1,17 @@
 
 rem @echo off
 
-SET JAVA_HOME="C:\Javajdk"
-SET JDK_HOME=%JAVA_HOME%
-SET JRE_HOME="C:\JavaJre"
-SET CLASSPATH=".;%JAVA_HOME%\lib;%JAVA_HOME%\jre\lib
-SET PATH=%PATH%;%JAVA_HOME%\bin;
+rem A newer version of the JRE made setting these
+rem unnecessary.  
+rem SET JAVA_HOME="C:\Javajdk"
+rem SET JDK_HOME=%JAVA_HOME%
+rem SET JRE_HOME="C:\Javajdk\jre"
+
+rem SET CLASSPATH=".;%JAVA_HOME%\lib;%JAVA_HOME%\jre\lib
+rem SET PATH=%PATH%;%JAVA_HOME%\bin;
 
 cd \Eric\CodeEditorJava
 
 Java MainApp
 
 rem pause
-
