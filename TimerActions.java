64dffa267f73abda6268d8de392da4849237e882
Eric Chauvin <chauvineric4@gmail.com>
2019-08-29 13:13:47 -0600

    Update TimerActions.java

--- TimerActions.java
+++ TimerActions.java
@@ -4,7 +4,6 @@
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-// import java.awt.event.KeyEvent;
 
 
 
@@ -25,26 +24,15 @@ public class TimerActions implements ActionListener
     {
     try
     {
-
-    mApp.showStatus( "Timer event happend." );
-
-    // String command = event.getActionCommand();
-    // mApp.showStatus( "ActionEvent Command is: " + command );
-
-
-    //   mApp.editSelectAll();
-
-
+    mApp.keyboardTimerEvent();
     }
     catch( Exception e )
       {
       mApp.showStatus( "Exception in TimerActions.actionPerformed()." );
       mApp.showStatus( e.getMessage() );
       }
-
     }
 
 
 
   }
-
