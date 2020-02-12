503704114d92c558b700aaa0ee03a13a6c8114a5
Eric Chauvin <chauvineric4@gmail.com>
2020-02-12 11:41:26 -0700

    Create Markers.java

---
+++ Markers.java
@@ -0,0 +1,49 @@
+// Copyright Eric Chauvin 2018 - 2020.
+
+
+// Miscellaneous Symbols (2600–26FF)
+// Dingbats (2700–27BF)
+// Miscellaneous Symbols and Arrows (2B00–2BFF)
+
+
+
+  public class Markers
+  {
+  // Marker symbols go from 0x2700 to 0x27BF.
+  public static final char SlashStar = (char)0x270A;
+  public static final char StarSlash = (char)0x270B;
+
+
+/*
+  public static final char EscapedSingleQuote = (char)0x2700;
+
+  internal const char EscapedDoubleQuote = (char)0x2701;
+  internal const char Begin = (char)0x2702;
+  internal const char End = (char)0x2703;
+  internal const char TypeString = (char)0x2704;
+  internal const char TypeChar = (char)0x2705;
+  internal const char TypeNumber = (char)0x2706;
+  internal const char TypeIdentifier = (char)0x2707;
+  internal const char ErrorPoint = (char)0x2708;
+  internal const char TypeLineNumber = (char)0x2709;
+  internal const char DoubleSlash = (char)0x270C;
+  internal const char TypeOperator = (char)0x270D;
+  internal const char EscapedSlash = (char)0x270E;
+  internal const char QuoteAsSingleCharacter = (char)0x270F;
+*/
+
+
+/*
+  internal static bool IsMarker( char TestChar )
+    {
+    int Value = (int)TestChar;
+    if( (Value >= 0x2700) && (Value <= 0x27BF))
+      return true;
+
+    return false;
+    }
+*/
+
+
+
+  }
